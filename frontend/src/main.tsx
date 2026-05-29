dfecf2938f68c0eed1d048debdf97248e843c9d5
bluejals13 <yoriya10113@gmail.com>
2026-05-29 22:37:09 +0900

    22:36
    authprovider 부분 설정 안하면 오류

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.css'
-import './App.css'
+import "./index.css";
+import "./App.css";
 
-import App from './App'
+import App from "./App";
+import { AuthProvider } from "./components/AuthContext";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
