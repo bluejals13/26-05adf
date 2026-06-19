d04bb12e5e7aac3812ebf3448add9fa0bddc3e34
bluejals13 <yoriya10113@gmail.com>
2026-06-19 17:25:28 +0900

    Add files via upload

---
+++ APMS.SR/dev/frontend/src/queries/useUsers.ts
@@ -0,0 +1,11 @@
+// queries/useUsers.ts			// 관리자 페이지 용 쿼리
+
+import { useQuery } from "@tanstack/react-query";
+import { userApi } from "../api/user.api";
+
+export function useUsers() {
+  return useQuery({
+    queryKey: ["users"],
+    queryFn: userApi.getUsers,
+  });
+}
