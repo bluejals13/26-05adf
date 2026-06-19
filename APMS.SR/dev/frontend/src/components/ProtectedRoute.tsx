837cd127d5091219b7745f441e28bff6efb80f1c
bluejals13 <yoriya10113@gmail.com>
2026-06-20 03:40:39 +0900

    Update ProtectedRoute.tsx

--- APMS.SR/dev/frontend/src/components/ProtectedRoute.tsx
+++ APMS.SR/dev/frontend/src/components/ProtectedRoute.tsx
@@ -3,10 +3,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store/auth.store";
 
+
+
 export default function ProtectedRoute() {
-  const token = useAuthStore((s) => s.token);
-
+  //const token = useAuthStore((s) => s.token);
+  const { token } = useAuthStore();
+  if (token === undefined) return null; // bootstrap 대기
   if (!token) return <Navigate to="/login" replace />;
-
+  
   return <Outlet />;
 }
