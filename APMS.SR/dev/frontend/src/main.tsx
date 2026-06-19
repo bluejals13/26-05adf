b3c3078c2c10a73ab3ec99f7a67d32eb2d462531
bluejals13 <yoriya10113@gmail.com>
2026-06-19 15:49:36 +0900

    Update main.tsx

--- APMS.SR/dev/frontend/src/main.tsx
+++ APMS.SR/dev/frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./queryClient";
+
 import "./index.css";
 import "./App.css";
 
@@ -8,6 +11,8 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <App />
+    </QueryClientProvider>
   </StrictMode>
 );
