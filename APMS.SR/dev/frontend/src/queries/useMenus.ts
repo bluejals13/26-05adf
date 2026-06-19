04c7e7204ac47916486bea3d111a02ed63e95f2c
bluejals13 <yoriya10113@gmail.com>
2026-06-19 18:14:35 +0900

    Add files via upload

---
+++ APMS.SR/dev/frontend/src/queries/useMenus.ts
@@ -0,0 +1,12 @@
+// queries/useMenus.ts			//서버 상태 관리
+
+import { useQuery } from "@tanstack/react-query";
+import { menuApi } from "../api/menu.api";
+
+export function useMenus() {
+  return useQuery({
+    queryKey: ["menus"],
+    queryFn: menuApi.getMenus,
+    staleTime: 1000 * 60,
+  });
+}
