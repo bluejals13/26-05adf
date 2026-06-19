823ce087c8c8cca18668c233db7e1b965dbbd694
bluejals13 <yoriya10113@gmail.com>
2026-06-20 01:34:36 +0900

    Update AdminRoute.tsx

--- APMS.SR/dev/frontend/src/components/AdminRoute.tsx
+++ APMS.SR/dev/frontend/src/components/AdminRoute.tsx
@@ -5,7 +5,11 @@ import FullPageSpinner from "./loading/FullPageSpinner";
 import { useMe } from "../queries/useMe";
 import { usePermissions } from "../auth/hooks/usePermissions";
 import type { Permission } from "../auth/auth.types";
+import type { User } from "../auth/auth.types";
 
+type Props = {
+  me?: User;
+};
 // 필요하면 string 기반으로
 type AdminRouteProps = { permission?: Permission; };
 
