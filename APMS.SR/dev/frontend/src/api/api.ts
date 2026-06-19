c4d5523c6c2432ab0e6732fd5df7e4e6254b57e9
bluejals13 <yoriya10113@gmail.com>
2026-06-20 06:55:38 +0900

    Update api.ts

--- APMS.SR/dev/frontend/src/api/api.ts
+++ APMS.SR/dev/frontend/src/api/api.ts
@@ -1,13 +1,13 @@
 // api.ts HTTP만
 
-import { http } from "../api";
+//import { http } from "../api";
 
-export const http = {
-  get: <T>(url: string) => request<T>(url),
+//export const http = {
+  //get: <T>(url: string) => request<T>(url),
 
-  post: <T>(url: string, body?: unknown) =>
-    request<T>(url, {
-      method: "POST",
-      body: body ? JSON.stringify(body) : undefined,
-    }),
-};
+  //post: <T>(url: string, body?: unknown) =>
+    //request<T>(url, {
+      //method: "POST",
+      //body: body ? JSON.stringify(body) : undefined,
+    //}),
+//};
