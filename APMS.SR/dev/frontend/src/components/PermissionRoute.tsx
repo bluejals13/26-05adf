588d130fdb49f62c504f183fe8fa0079f4f0be70
bluejals13 <yoriya10113@gmail.com>
2026-06-20 01:50:31 +0900

    Update PermissionRoute.tsx

--- APMS.SR/dev/frontend/src/components/PermissionRoute.tsx
+++ APMS.SR/dev/frontend/src/components/PermissionRoute.tsx
@@ -8,10 +8,6 @@ import type { Permission } from "../auth/auth.types";
 
 import type { User } from "../auth/auth.types";
 
-type Props = {
-  me?: User;
-};
-
 export function PermissionRoute({ permission }: { permission: Permission }) {
   const { user, isLoading } = useAuth();
 
