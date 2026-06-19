abc0f03ef15855548141a796039f0292113bf787
bluejals13 <yoriya10113@gmail.com>
2026-06-20 02:49:55 +0900

    Update useMenus.ts

--- APMS.SR/dev/frontend/src/queries/useMenus.ts
+++ APMS.SR/dev/frontend/src/queries/useMenus.ts
@@ -2,15 +2,14 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { menuApi } from "../api/menu.api";
-
-import { useAuth } from "../auth/hooks/useAuth";
+import { useAuthStore } from "../store/auth.store";
 
 export function useMenus() {
-  const { user, isLoading } = useAuth();
+  const token = useAuthStore((s) => s.token);
 
   return useQuery({
     queryKey: ["menus"],
     queryFn: menuApi.getMenus,
-    enabled: !!user && !isLoading,
+    enabled: !!token,
   });
 }
