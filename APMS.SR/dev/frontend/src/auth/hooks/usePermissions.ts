40bb048f55324a62bc83686f8da6f40140c692d5
bluejals13 <yoriya10113@gmail.com>
2026-06-19 22:20:43 +0900

    Update usePermissions.ts

--- APMS.SR/dev/frontend/src/auth/hooks/usePermissions.ts
+++ APMS.SR/dev/frontend/src/auth/hooks/usePermissions.ts
@@ -1,15 +1,18 @@
 // hooks/usePermissions.ts	// 타입 > 롤 퍼미션 > 훅  :  권한 계산 관리
 
-import type { User, Permission, Role } from "../auth.types";
+import type { User } from "../auth.types";
 
 export function usePermissions(user?: User) {
+  const permissions = user?.permissions ?? [];
+  const roles = user?.roles ?? [];
+
   return {
-    hasPermission: (perm: Permission) =>
-      user?.permissions.includes(perm) ?? false,
+    hasPermission: (perm: string) =>
+      permissions.includes(perm),
 
-    hasRole: (role: Role) =>
-      user?.roles.includes(role) ?? false,
+    hasRole: (role: string) =>
+      roles.includes(role),
 
-    isAdmin: user?.roles.includes("ADMIN") ?? false,
+    isAdmin: roles.includes("ADMIN"),
   };
 }
