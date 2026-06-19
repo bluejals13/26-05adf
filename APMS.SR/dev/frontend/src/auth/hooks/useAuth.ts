a5f4eb2663e74ea51b2b888e8c1fc0c778bf3f6a
bluejals13 <yoriya10113@gmail.com>
2026-06-19 16:03:59 +0900

    Update useAuth.ts

--- APMS.SR/dev/frontend/src/auth/hooks/useAuth.ts
+++ APMS.SR/dev/frontend/src/auth/hooks/useAuth.ts
@@ -1,8 +1,8 @@
 // hooks/useAuth.ts		// 스토어 > 쿼리 > 훅  :  컴포지션 관리
 
-import { useMe } from "../queries/useMe";
-import { useAuthStore } from "../store/auth.store";
-import { authService } from "../auth/auth.service";
+import { useMe } from "../../queries/useMe";
+import { useAuthStore } from "../../store/auth.store";
+import { authService } from "../auth.service";
 
 export function useAuth() {
   const token = useAuthStore((s) => s.token);
