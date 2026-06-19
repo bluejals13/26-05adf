093cc939aeed266ea30074bfb950eeb3a7b2ed6a
bluejals13 <yoriya10113@gmail.com>
2026-06-19 23:25:47 +0900

    Update auth.types.ts

--- APMS.SR/dev/frontend/src/auth/auth.types.ts
+++ APMS.SR/dev/frontend/src/auth/auth.types.ts
@@ -1,6 +1,6 @@
 // auth/auth.types.ts						// 각 롤 퍼미션 유저 타입 관리
 
-export type Role = "ADMIN" | "USER" | "MANAGER";
+export type Role = string;
 export type Permission = string;
 
 export type User = {
