cb82019a5204cf97d9841431495362c6ca8929d8
bluejals13 <yoriya10113@gmail.com>
2026-06-22 02:42:32 +0900

    Update TokenResponse.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/iam/user/dto/TokenResponse.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/iam/user/dto/TokenResponse.java
@@ -1,10 +1,6 @@
 package com.example.demo.iam.user.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
 public record TokenResponse(
         String accessToken,
         String refreshToken
