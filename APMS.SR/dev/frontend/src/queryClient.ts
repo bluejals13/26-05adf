b7374510d0a2e511c8947619e4703a6045bab24c
bluejals13 <yoriya10113@gmail.com>
2026-06-22 22:45:18 +0900

    Update queryClient.ts

--- APMS.SR/dev/frontend/src/queryClient.ts
+++ APMS.SR/dev/frontend/src/queryClient.ts
@@ -1,4 +1,4 @@
-// src/queryClient.ts
+// src/queryClient.ts    // 일반 고객용 계시판 혹은 상품 , 댓글 같은 잡다 기능용 쿼리
 
 import { QueryClient } from "@tanstack/react-query";
 
@@ -7,7 +7,7 @@ export const queryClient = new QueryClient({
     queries: {
       retry: 1,
       refetchOnWindowFocus: false,
-      staleTime: 1000 * 60, // 1분
+      staleTime: 1000 * 60, // 1분 간격으로
     },
   },
 });
