0ad584bd8d38a0fc09b7b1af4420700b8cf8f015
bluejals13 <yoriya10113@gmail.com>
2026-06-23 02:06:55 +0900

    Update UserAdminPage.tsx

--- APMS.SR/dev/frontend/src/pages/FUsAd/UserAdminPage.tsx
+++ APMS.SR/dev/frontend/src/pages/FUsAd/UserAdminPage.tsx
@@ -62,7 +62,7 @@ export default function UserAdminPage() {
   
   //const permissions = user.permissions ?? [];
   
-  onsole.log("users data:", data);
+  console.log("users data:", data);
   console.log("loading:", isLoading);
   console.log("error:", error);
 
