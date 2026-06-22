8c8706b6fcc8471672b45117e5518a55f1014966
bluejals13 <yoriya10113@gmail.com>
2026-06-22 23:21:06 +0900

    Update auth.store.ts

--- APMS.SR/dev/frontend/src/store/auth.store.ts
+++ APMS.SR/dev/frontend/src/store/auth.store.ts
@@ -1,35 +1,24 @@
 // store/auth.store.ts	// 소비자 상태 (Zustand) 관리 	setToken
 
 import { create } from "zustand";
-import { authStorage } from "../auth/auth.storage";
+import { persist } from "zustand/middleware";
+import { STORAGE_KEYS } from "../constants/keys";
 
 type AuthState = {
   token: string | null;
-
-  setToken: (token: string | null) => void;
-
+  setToken: (t: string | null) => void;
   logout: () => void;
-  hydrate: () => void;
 };
 
-export const useAuthStore = create<AuthState>((set) => ({
-  token: null,
-
-  setToken: (token) => {
-    if (token) authStorage.set(token);
-    else authStorage.clear();
-
-    set({ token });
-  },
-
-  logout: () => {
-    authStorage.clear();
-    set({ token: null });
-  },
-
-  hydrate: () => {
-    set({
-      token: authStorage.get(),
-    });
-  },
-}));
+export const useAuthStore = create(
+  persist<AuthState>(
+    (set) => ({
+      token: null,
+      setToken: (t) => set({ token: t }),
+      logout: () => set({ token: null }),
+    }),
+    {
+      name: STORAGE_KEYS.auth,
+    }
+  )
+);
