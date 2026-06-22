d8851a9f97a257e2e9ffc33254a888b8dda5474f
bluejals13 <yoriya10113@gmail.com>
2026-06-22 23:55:33 +0900

    Update useUsers.ts

--- APMS.SR/dev/frontend/src/queries/useUsers.ts
+++ APMS.SR/dev/frontend/src/queries/useUsers.ts
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { userApi } from "../api/user.api";
 // import { useAuthStore } from "../store/auth.store";
 
+export function useUsers(token: string | null) {
   return useQuery({
     queryKey: ["users"],
     queryFn: userApi.getUsers,
