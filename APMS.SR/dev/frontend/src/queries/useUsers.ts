91496f4ba15266c307f13ecbb5e1204dc9925de5
bluejals13 <yoriya10113@gmail.com>
2026-06-22 23:50:08 +0900

    Update useUsers.ts

--- APMS.SR/dev/frontend/src/queries/useUsers.ts
+++ APMS.SR/dev/frontend/src/queries/useUsers.ts
@@ -5,10 +5,14 @@ import { userApi } from "../api/user.api";
 // import { useAuthStore } from "../store/auth.store";
 
 export function useUsers() {
-
+const authReady =
+  !!token &&
+  isLoggedIn === true &&
+  isLoading === false;
 
   return useQuery({
     queryKey: ["users"],
     queryFn: userApi.getUsers,
+    enabled: authReady,
   });
 }
