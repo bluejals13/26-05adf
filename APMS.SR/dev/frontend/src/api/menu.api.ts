a9b505cca561a691a52afcd485c5d47bbebd1ca5
bluejals13 <yoriya10113@gmail.com>
2026-06-23 14:59:49 +0900

    Update menu.api.ts

--- APMS.SR/dev/frontend/src/api/menu.api.ts
+++ APMS.SR/dev/frontend/src/api/menu.api.ts
@@ -1,11 +1,13 @@
 // api/menu.api.ts		//메뉴 용 api
 
 import { http } from "../api/http";
-import type { Menu } from "../auth/auth.types";
-import type { MenuRequest } from "../auth/auth.types";
+import type { Menu, MenuRequest } from "../auth/auth.types";
 
 export const menuApi = {
-  getMenus: () => http.get<Menu[]>("/api/admin/menus"),
+  getMenus: async (): Promise<Menu[]> => {
+    const list = await http.get<Menu[]>("/api/admin/menus");
+    return list;
+  },
 
   createMenu: (data: MenuRequest) =>
     http.post("/api/admin/menus", data),
