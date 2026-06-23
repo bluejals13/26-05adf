c241dd4a9f9bacaa8152691b18a22fb689eaad03
bluejals13 <yoriya10113@gmail.com>
2026-06-23 11:03:44 +0900

    useUsers.ts 업데이트

--- APMS.SR/dev/frontend/src/queries/useUsers.ts
+++ APMS.SR/dev/frontend/src/queries/useUsers.ts
@@ -7,15 +7,19 @@ export const userKeys = {
   all: ["users"] as const,
 };
 
-export function useUsers() {
+export function useUsers(options?: {
+  enabled?: boolean;
+}) {
   return useQuery({
     queryKey: userKeys.all,
     queryFn: userApi.getUsers,
 
+    enabled: options?.enabled ?? true,
+
     initialData: [],
     retry: 1,
 
-    staleTime: 1000 * 10, // 10s
+    staleTime: 1000 * 10,
     refetchOnWindowFocus: true,
   });
 }
