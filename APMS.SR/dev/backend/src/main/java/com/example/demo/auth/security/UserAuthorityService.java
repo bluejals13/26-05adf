d501fb24333dfed06d4a3a22cef2531064a19dda
bluejals13 <yoriya10113@gmail.com>
2026-06-25 23:55:41 +0900

    Update and rename UserDetailsService.java to UserAuthorityService.java

---
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/UserAuthorityService.java
@@ -0,0 +1,38 @@
+package com.example.demo.auth.security;
+
+import com.example.demo.iam.user.domain.User;
+import com.example.demo.iam.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserAuthorityService {
+
+    private final UserRepository userRepository;
+
+    public List<GrantedAuthority> getAuthorities(Long userId) {
+
+        User user = userRepository.findWithRolesAndPermissionsById(userId)
+                .orElseThrow();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        user.getRoles().forEach(r ->
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + r.getName()))
+        );
+
+        user.getRoles().forEach(r ->
+                r.getPermissions().forEach(p ->
+                        authorities.add(new SimpleGrantedAuthority(p.getName()))
+                )
+        );
+
+        return authorities;
+    }
+}
