57ab1aa9831002758ff328663a688f541d4bde38
bluejals13 <yoriya10113@gmail.com>
2026-06-25 16:18:36 +0900

    Update AuthService.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/AuthService.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/AuthService.java
@@ -7,6 +7,8 @@ import com.example.demo.iam.user.dto.LoginResult;
 import com.example.demo.iam.user.dto.TokenResponse;
 import com.example.demo.iam.user.repository.UserRepository;
 
+import com.example.demo.common.exception.GlobalExceptionHandler;
+
 import io.jsonwebtoken.Claims;
 
 import lombok.RequiredArgsConstructor;
