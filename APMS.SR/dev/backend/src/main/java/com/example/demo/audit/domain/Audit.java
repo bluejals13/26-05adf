36b0469b00d6f739fcdb1acff480d94066e2a6ca
bluejals13 <yoriya10113@gmail.com>
2026-06-25 21:55:54 +0900

    Update Audit.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/audit/domain/Audit.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/audit/domain/Audit.java
@@ -16,30 +16,36 @@ public class Audit {
     private Long userId;      // 누가 했는지
     
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private AuditAction action;   // 무엇을 했는지 (CREATE, DELETE 등)
     
     private String targetType; // 대상 타입
 
     private Long targetId;    // 대상 ID
 
-    private String result;    // 작업 성공/실패
+    private String beforeValue;    // 전 결과
+
+    private String afterValue;     // 후 결과
     
     private LocalDateTime createdAt;
     
     public static Audit create(
-        Long userId,
-        AuditAction action,
-        Long targetId
+            Long userId,
+            AuditAction action,
+            String targetType,
+            Long targetId,
+            String beforeValue,
+            String afterValue
     ) {
         Audit audit = new Audit();
 
         audit.userId = userId;
         audit.action = action;
+        audit.targetType = targetType;
         audit.targetId = targetId;
+        audit.beforeValue = beforeValue;
+        audit.afterValue = afterValue;
         audit.createdAt = LocalDateTime.now();
 
         return audit;
     }
-    
 }
