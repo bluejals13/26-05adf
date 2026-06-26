c8ed1dc5a70fd3891a25f5ca61f392a7004d4dab
bluejals13 <yoriya10113@gmail.com>
2026-06-26 23:52:00 +0900

    Update JwtAuthenticationFilter.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/JwtAuthenticationFilter.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/JwtAuthenticationFilter.java
@@ -69,12 +69,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
-        if (token == null || token.isBlank()) {
+        String token = header.substring(7);     
+        
+        if (token == null || token.isBlank()) {    // 3.5 토큰 null 시 회원가입 의 경우 
             filterChain.doFilter(request, response);
             return;
         }
-        String token = header.substring(7);     
-        
         
         try {       
             // 4. JWT 검증 (먼저)
