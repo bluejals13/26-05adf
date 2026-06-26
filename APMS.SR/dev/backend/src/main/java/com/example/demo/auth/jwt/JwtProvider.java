98e6332fa09f1b442e749bc0260ca90c014b882c
bluejals13 <yoriya10113@gmail.com>
2026-06-26 14:10:27 +0900

    Update JwtProvider.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/auth/jwt/JwtProvider.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/jwt/JwtProvider.java
@@ -56,7 +56,7 @@ public class JwtProvider {    // 각 토큰 제공 파일
 
     return Jwts.builder()
             .setSubject(String.valueOf(userId))
-            //.setId(jti) // 🔥 핵심
+            .setId(jti) // 🔥 핵심
             .claim("username", username) 
             //.claim("type", "access") // 나중에 필터 에서 string = claims.get("type" ~~~~) \n if (!"access".equals(type))
             .setIssuedAt(now)
@@ -101,7 +101,7 @@ public class JwtProvider {    // 각 토큰 제공 파일
     }
 
     // 4. 중복 등 예외처리 시 검증
-    public boolean validateToken(String token) {
+    public boolean validateToken(String token) { //expired / malformed / signature error 구분 추가
         try {
             parseClaims(token);
             return true;
