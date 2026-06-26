72aaad7c3087629e2033b2dc5f536518c2af946c
bluejals13 <yoriya10113@gmail.com>
2026-06-26 22:06:38 +0900

    Update CustomUserPrincipal.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/CustomUserPrincipal.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/CustomUserPrincipal.java
@@ -1,55 +1,10 @@
 package com.example.demo.auth.security;
 
-import com.example.demo.iam.user.domain.User;
-
-import org.springframework.security.core.GrantedAuthority;
-
 import lombok.AllArgsConstructor;
-import java.util.Collection;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class CustomUserPrincipal {
-
     private final Long userId;
-    public CustomUserPrincipal(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return user.getAuthorities();
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getUsername();
-    }
-
-    // 👇 핵심: ACTIVE 아니면 로그인 불가
-    @Override
-    public boolean isEnabled() {
-        return user.getStatus() == UserStatus.ACTIVE;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {        // 로그인 불가
-        return user.getStatus() != UserStatus.SUSPENDED;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {        // 해당 계정 로그인 불가 및 현재 세션 강제 로그아웃
-        return user.getStatus() != UserStatus.DELETED;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
 }
