8e95428087f0b0c2bbe3c9d88a5450709da11ff3
bluejals13 <yoriya10113@gmail.com>
2026-06-26 15:18:31 +0900

    Update TokenBlacklistService.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/TokenBlacklistService.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/TokenBlacklistService.java
@@ -29,7 +29,7 @@ public class TokenBlacklistService {    // 이미 발급된 토큰 차단    “
         
         String jti = jwtProvider.getJti(token);
         
-        redisTemplate.opsForValue().get("blacklist:" + jti) != null
+        redisTemplate.opsForValue().get("blacklist:" + jti) != null;
     }
 
     // 체크
