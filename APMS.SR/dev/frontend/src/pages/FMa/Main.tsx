5e024282108bcd7d86ddee8b8fa066c537e26806
bluejals13 <yoriya10113@gmail.com>
2026-07-03 17:29:41 +0900

    Update Main.tsx

--- APMS.SR/dev/frontend/src/pages/FMa/Main.tsx
+++ APMS.SR/dev/frontend/src/pages/FMa/Main.tsx
@@ -32,19 +32,26 @@ export default function Main() {
         {hasPermission("PERMISSION_READ") && (
         <div className="card" onClick={() => navigate("/permission")}>
           <h2>🔐 Permission</h2>
-          <p>권한 관리 페이지</p>
+          <p>작업 관리 페이지</p>
         </div> )}
         {hasPermission("USER_READ") && (
-        <div className="card" onClick={() => navigate("/user")}>
+        <div className="card" onClick={() => navigate("/Admin")}>
           <h2>👤 User Admin</h2>
-          <p>유저 관리 페이지</p>
+          <p>계정 관리 페이지</p>
+        </div> )}
+        {hasPermission("ROLE_READ") && (
+        <div className="card" onClick={() => navigate("/Role")}>
+          <h2>👤 User Admin</h2>
+          <p>권한등급 관리 페이지</p>
         </div> )}
         
         <div className="card debug" onClick={() => navigate("/about")}>
           <h2>🧪 Debug</h2>
           <p>RBAC / Auth 확인</p>
         </div> 
-
+        
+        
+        
       </section>
 
       {/* FOOTER */}
