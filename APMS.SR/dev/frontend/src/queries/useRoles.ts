9f8740fd06401a302da3ce07f81c1886d9ea43ae
bluejals13 <yoriya10113@gmail.com>
2026-07-03 17:03:50 +0900

    Create useRoles.ts

---
+++ APMS.SR/dev/frontend/src/queries/useRoles.ts
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchRoles } from "./role.api";
+
+export const useRoles = () => {
+  return useQuery({
+    queryKey: ["roles"],
+    queryFn: fetchRoles,
+  });
+};
