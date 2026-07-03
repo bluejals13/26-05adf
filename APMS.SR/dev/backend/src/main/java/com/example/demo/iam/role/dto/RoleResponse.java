d492dfe731feef16aa0d2814261fff2cc045debd
bluejals13 <yoriya10113@gmail.com>
2026-07-04 00:14:09 +0900

    Update RoleResponse.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/iam/role/dto/RoleResponse.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/iam/role/dto/RoleResponse.java
@@ -14,12 +14,14 @@ public class RoleResponse {
 
     private Long id;
     private String name;
+    private String description; // ⭐ 추가
     private Set<PermissionResponse> permissions;
 
     public static RoleResponse from(com.example.demo.iam.role.domain.Role role) {
         return RoleResponse.builder()
                 .id(role.getId())
                 .name(role.getName())
+                .description(role.getDescription()) // ⭐ 추가
                 .permissions(
                         role.getPermissions().stream()
                                 .map(PermissionResponse::from)
