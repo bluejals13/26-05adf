83064b8f8613819ec7ce3727de5726a192154527
bluejals13 <yoriya10113@gmail.com>
2026-07-03 19:17:44 +0900

    Update useRoles.ts

--- APMS.SR/dev/frontend/src/queries/useRoles.ts
+++ APMS.SR/dev/frontend/src/queries/useRoles.ts
@@ -2,7 +2,7 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { fetchRoles } from "../api/role.api";
-import type { Role } from "../auth/auth.types";
+import type { Role } from "../queries/role";
 
 export const useRoles = () =>
   useQuery<Role[]>({
