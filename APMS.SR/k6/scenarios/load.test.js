091d273c6c55196575fb90a901922f0d29b8426f
bluejals13 <yoriya10113@gmail.com>
2026-07-04 20:36:07 +0900

    Update load.test.js

--- APMS.SR/k6/scenarios/load.test.js
+++ APMS.SR/k6/scenarios/load.test.js
@@ -1,7 +1,9 @@
 // scenarios/load.test.js
 
-import adminFlow from "./admin.flow.js";
 import userFlow from "./user.flow.js";
+import readFlow from "./read.flow.js";
+import adminFlow from "./admin.flow.js";
+
 //import { thresholds } from "../config/thresholds.js";
 
 export const options = { vus: 50,
@@ -10,6 +12,7 @@ export const options = { vus: 50,
 };
 
 export default function () {
-    adminFlow();
     userFlow();
+    readFlow();
+    adminFlow();  
 }
