4422dfe2f06de94a92cd565fd12ac788b1069c03
bluejals13 <yoriya10113@gmail.com>
2026-07-04 16:56:04 +0900

    Update request.js

--- APMS.SR/k6/core/request.js
+++ APMS.SR/k6/core/request.js
@@ -1,26 +1,15 @@
 // k6/core/request.js		통신규격
 
-import http from "k6/http";
-import { config } from "../config/env.js";
 import { getToken } from "./auth.js";
 
-const BASE_URL = config.baseUrl;
-
-function buildHeaders(extraHeaders = {}) {
+export default function request(method, path, body, options = {}) {
     const token = getToken();
 
-    return {
-        "Content-Type": "application/json",
-        ...extraHeaders,
-        ...(token ? { Authorization: `Bearer ${token}` } : {}),
-    };
-}
-
-export default function request(method, path, body = null, options = {}) {
-    const payload = body == null ? null : JSON.stringify(body);
-
-    return http.request(method, `${BASE_URL}${path}`, payload, {
+    return http.request(method, path, body, {
         ...options,
-        headers: buildHeaders(options.headers),
+        headers: {
+            ...(options.headers || {}),
+            ...(token ? { Authorization: `Bearer ${token}` } : {})
+        }
     });
 }
