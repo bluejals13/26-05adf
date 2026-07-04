18eab5bf911d044e469243823fb62a638ded186c
bluejals13 <yoriya10113@gmail.com>
2026-07-04 21:39:52 +0900

    Update run-full-test.sh

--- APMS.SR/k6/run-full-test.sh
+++ APMS.SR/k6/run-full-test.sh
@@ -6,11 +6,6 @@ sleep 5
 
 echo "Starting k6 test..."
 
-k6 run \
-  --vus 50 \
-  --duration 2m \
-  --env BASE_URL=http://localhost:8080 \
-  --out experimental-prometheus-rw \
-  core/active.js
+./run-k6.sh
 
 echo "Test finished"
