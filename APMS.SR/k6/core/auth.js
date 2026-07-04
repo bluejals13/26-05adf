cbc3d23f2ebb09374a8ab0da9d7c8fa4361f0610
bluejals13 <yoriya10113@gmail.com>
2026-07-04 16:48:25 +0900

    Update auth.js

--- APMS.SR/k6/core/auth.js
+++ APMS.SR/k6/core/auth.js
@@ -2,9 +2,24 @@
 
 import request from "./request.js";
 
+let token = null;
+
 export function login(username, password) {
-    return request("POST", "/api/auth/login", {
+    const res = request("POST", "/api/auth/login", {
         username,
         password,
     });
+
+    token = res.json().token;
+    return token;
+}
+
+export function getToken() {
+    return token;
+}
+
+export function refreshToken() {
+    const res = request("POST", "/api/auth/refresh");
+    token = res.json().token;
+    return token;
 }
