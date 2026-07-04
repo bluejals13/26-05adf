fd50a9d0c9b2951c0822e68f21ce6a501799c255
bluejals13 <yoriya10113@gmail.com>
2026-07-04 16:46:50 +0900

    Update auth.js

--- APMS.SR/k6/core/auth.js
+++ APMS.SR/k6/core/auth.js
@@ -1,6 +1,6 @@
 // k6/core/auth.js		인증
 
-import { request } from "./request.js";
+import request from "./request.js";
 
 export function login(username, password) {
     return request("POST", "/api/auth/login", {
