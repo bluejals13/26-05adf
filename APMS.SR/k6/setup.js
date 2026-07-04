0057bf664011a94ae5719912e3121334261f40cb
bluejals13 <yoriya10113@gmail.com>
2026-07-04 16:33:48 +0900

    Update setup.js

--- APMS.SR/k6/setup.js
+++ APMS.SR/k6/setup.js
@@ -2,7 +2,7 @@
 
 import { login } from "./core/auth.js";
 import active from "./core/active.js";
-import { request } from "./core/request.js";
+//import { request } from "./core/request.js";
 
 export function setup() {
     login();
