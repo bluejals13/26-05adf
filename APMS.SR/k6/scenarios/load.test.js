dad8818016111a8e462c915ac2ed7c10a68574b6
bluejals13 <yoriya10113@gmail.com>
2026-07-04 17:11:49 +0900

    Update load.test.js

--- APMS.SR/k6/scenarios/load.test.js
+++ APMS.SR/k6/scenarios/load.test.js
@@ -2,6 +2,12 @@
 
 import adminFlow from "./admin.flow.js";
 import userFlow from "./user.flow.js";
+//import { thresholds } from "../config/thresholds.js";
+
+export const options = { vus: 50,
+    duration: "2m",
+    thresholds,
+};
 
 export default function () {
     adminFlow();
