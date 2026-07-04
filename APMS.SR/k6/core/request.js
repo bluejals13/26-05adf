0d522c17285a3c8d0a8237f09cc7ae9b75f544f0
bluejals13 <yoriya10113@gmail.com>
2026-07-04 21:36:26 +0900

    Update request.js

--- APMS.SR/k6/core/request.js
+++ APMS.SR/k6/core/request.js
@@ -4,16 +4,14 @@ import http from "k6/http";
 import { config } from "../config/env.js";
 
 export default function request(method, path, body, options = {}, token) {
+    const payload = body ? JSON.stringify(body) : null;
+    
     const headers = {
         "Content-Type": "application/json",
-        ...(token ? { Authorization: `Bearer ${token}` } : {}),
-        ...(options.headers || {}),
+        ...(options.headers ?? {}),
+        ...(token && { Authorization: `Bearer ${token}`, }),
     };
 
-    const payload = body ? JSON.stringify(body) : null;
-
     return http.request(method, `${config.baseUrl}${path}`, payload, {
-        ...options,
-        headers,
-    });
+        ...options, headers, });
 }
