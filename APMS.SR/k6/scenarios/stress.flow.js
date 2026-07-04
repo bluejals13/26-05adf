68aae3b09b37d43f270cf30bb5c22174c08d6993
bluejals13 <yoriya10113@gmail.com>
2026-07-04 20:38:21 +0900

    Update stress.flow.js

--- APMS.SR/k6/scenarios/stress.flow.js
+++ APMS.SR/k6/scenarios/stress.flow.js
@@ -1,17 +1,17 @@
 // scenarios/stress.flow.js
 
 import adminFlow from "./admin.flow.js";
+import userFlow from "./user.flow.js";
 
 export const options = {
     stages: [
-        { duration: "1m", target: 10 },
-        { duration: "2m", target: 50 },
-        { duration: "1m", target: 100 },
-        { duration: "1m", target: 0 },
-    ],
-    thresholds,
+      { duration: "1m", target: 50 },
+      { duration: "2m", target: 100 },
+      { duration: "1m", target: 200 },
+    ]
 };
 
 export default function () {
-    adminFlow();
+    adminFlow()
+    userFlow()
 }
