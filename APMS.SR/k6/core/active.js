65770ead0df17117920b576c93995082b717f9c8
bluejals13 <yoriya10113@gmail.com>
2026-07-04 21:31:09 +0900

    Update active.js

--- APMS.SR/k6/core/active.js
+++ APMS.SR/k6/core/active.js
@@ -2,17 +2,16 @@
 
 import { UserAPI } from "../api/user.api.js";
 
-export default function (data) {
-    const res = UserAPI.getUsers(data);
+export default function ({ token }) {
+    const res = UserAPI.getUsers(token);
 
-    if (res.status !== 200) {
-        console.error("getUsers failed:", res.status);
+    if (res.status !== 200) { console.error(`getUsers failed: ${res.status}`);
         return;
     }
 
     const users = res.json();
 
-    if (!users.length) return;
+    if (!users?.length) { return; }
 
     UserAPI.changeStatus(token, users[0].id, "ACTIVE");
 }
