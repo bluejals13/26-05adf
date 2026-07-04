99f53280a2321c31893ddf4eabe8b0825f1323a8
bluejals13 <yoriya10113@gmail.com>
2026-07-04 16:00:38 +0900

    Update auth.js

--- APMS.SR/k6/core/auth.js
+++ APMS.SR/k6/core/auth.js
@@ -1,61 +1,10 @@
 // k6/auth.js		인증
 
-import http from "k6/http";
-import { config } from "../config/env.js";
+import { request } from "../core/request.js";
 
-let tokenCache = null;
-let refreshTokenCache = null;
-let refreshing = false;
-
-export function login() {
-    const res = http.post(
-        `${config.baseUrl}/api/auth/login`,
-        JSON.stringify({
-            username: "admin",
-            password: "1234",
-        }),
-        {
-            headers: { "Content-Type": "application/json" },
-        }
-    );
-
-    const body = res.json();
-
-    tokenCache = body.accessToken;
-    refreshTokenCache = body.refreshToken;
-
-    return tokenCache;
-}
-
-export function getToken() {
-    if (!tokenCache) {
-        return login();
-    }
-    return tokenCache;
-}
-
-export function refreshToken() {
-    // 🔥 중복 refresh 방지
-    if (refreshing) {
-        return tokenCache;
-    }
-
-    refreshing = true;
-
-    const res = http.post(
-        `${config.baseUrl}/api/auth/refresh`,
-        JSON.stringify({
-            refreshToken: refreshTokenCache,
-        }),
-        {
-            headers: { "Content-Type": "application/json" },
-        }
-    );
-
-    const body = res.json();
-
-    tokenCache = body.accessToken;
-    refreshing = false;
-
-    return tokenCache;
+export function login(username, password) {
+    return request("POST", "/api/auth/login", {
+        username,
+        password,
+    });
 }
