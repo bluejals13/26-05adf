f3e60736f249a935f50218deb08f187352c6185d
bluejals13 <yoriya10113@gmail.com>
2026-07-04 21:41:00 +0900

    '

--- APMS.SR/k6/run-k6.sh
+++ APMS.SR/k6/run-k6.sh
@@ -5,25 +5,26 @@
 # =========================
 
 # 환경 변수
-export BASE_URL="http://localhost:8080"
+export BASE_URL="${BASE_URL:-http://localhost:8080}"
 
 # k6 실행 옵션
 VUS=${VUS:-50}
 DURATION=${DURATION:-2m}
-TEST=${TEST:-setup.js}
+ENTRY=${ENTRY:-run.js}
 
 echo "================================="
 echo "k6 Load Test Start"
-echo "TEST      : $TEST"
 echo "VUS       : $VUS"
 echo "DURATION  : $DURATION"
 echo "BASE_URL  : $BASE_URL"
 echo "================================="
 
 k6 run \
-  --vus $VUS \
-  --duration $DURATION \
-  --env BASE_URL=$BASE_URL \
-  $TEST
+  --vus "$VUS" \
+  --duration "$DURATION" \
+  --env BASE_URL="$BASE_URL" \
+  --out experimental-prometheus-rw \
+  "$ENTRY"
+
 
 
