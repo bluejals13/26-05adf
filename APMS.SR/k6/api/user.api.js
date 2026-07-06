7dc117b6ab7fa46c39187b0c8cd74eb22a6ad7e3
bluejals13 <yoriya10113@gmail.com>
2026-07-06 19:39:04 +0900

    Update user.api.js

--- APMS.SR/k6/api/user.api.js
+++ APMS.SR/k6/api/user.api.js
@@ -4,11 +4,17 @@ import request from "../core/request.js";
 
 export const UserAPI = {
     getUsers: (token) =>
-        request("GET", "/api/admin/users", token, null, { tags: { api: "getUsers" } } ),
+        request("GET", "/api/admin/users", 
+                token, null, 
+                { tags: { api: "getUsers" } } ),
 
     changeStatus: (token, id, status) =>
-        request("PATCH", `/api/admin/users/${id}/status`, token, { status }, {} ),
+        request("PATCH", `/api/admin/users/${id}/status`, 
+                token, { status }, 
+                { tags: { api: "changeStatus" } } ),
 
     deleteUser: (token, id) =>
-        request("DELETE", `/api/admin/users/${id}`, token, null, {} ),
+        request("DELETE", `/api/admin/users/${id}`, 
+                token, null, 
+                { tags: { api: "deleteUser" } } ),
 };
