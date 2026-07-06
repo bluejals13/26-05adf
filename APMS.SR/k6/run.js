04cd46d1e41fc7a8ce7ef5872cfe6c21b0e55f7a
bluejals13 <yoriya10113@gmail.com>
2026-07-06 20:53:09 +0900

    Update run.js

--- APMS.SR/k6/run.js
+++ APMS.SR/k6/run.js
@@ -11,14 +11,11 @@ import load from "./scenarios/load.test.js";
 export { setup };
 
 // 환경변수로 시나리오 선택
-const SCENA = __ENV.SCENA || "1"
-
-const scenario = __ENV.scenario || "user"
-
-const flows = { admin, user, read, load };
+//const SCENA = __ENV.SCENA || "1"
+//const scenario = __ENV.scenario || "user"
+//const flows = { admin, user, read, load };
 
 export default function (data) {
-    if ( SCENA == "1" ){
         
     const userRatio = Number(__ENV.USER_RATIO || 0.70);
     const readRatio = Number(__ENV.READ_RATIO || 0.27);
@@ -30,11 +27,7 @@ export default function (data) {
     else admin(data);
         
     }
-    else if ( SCENA == "2" ){
-    
-    flows[scenario](data);
-        
-    }
+    // flows[scenario](data); 
 }
 
 export const options = {
