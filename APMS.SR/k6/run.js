72fb2f7917394b972c7b0059e0b3110c2067af69
bluejals13 <yoriya10113@gmail.com>
2026-07-06 20:33:35 +0900

    Update run.js

--- APMS.SR/k6/run.js
+++ APMS.SR/k6/run.js
@@ -11,12 +11,23 @@ import load from "./scenarios/load.test.js";
 export { setup };
 
 // 환경변수로 시나리오 선택
-const scenario = __ENV.SCENARIO || "load";
+const scenario_1 = __ENV.1_SCENARIO || "user";
+const scenario_2 = __ENV.2_SCENARIO || "read";
+const scenario_3 = __ENV.3_SCENARIO || "admin";
 
 const flows = { admin, user, read, load };
 
 export default function (data) {
-    flows[scenario](data);
+    
+    const userRatio = Number(__ENV.USER_RATIO || 0.70);
+    const readRatio = Number(__ENV.READ_RATIO || 0.27);
+    const adminRatio = Number(__ENV.ADMIN_RATIO || 0.03);
+    
+    if (r < userRatio) scenario_1(data);
+    else if (r < userRatio + readRatio) read(data);
+    else admin(data);
+    
+    //flows[scenario](data);
 }
 
 export const options = {
