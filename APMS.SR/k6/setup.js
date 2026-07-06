1fd1d36527b319e770c453795a02bba53a31fc10
bluejals13 <yoriya10113@gmail.com>
2026-07-06 21:57:54 +0900

    Update setup.js

--- APMS.SR/k6/setup.js
+++ APMS.SR/k6/setup.js
@@ -5,7 +5,7 @@ import { login } from "./api/auth.api.js";
 export function setup() {
     const res = login("test", "1378");
     
-    if (res.status !== 200) { throw new Error("login failed"); }
+    if (res.status !== 200) { throw new Error("login failed " + res.status); }
     
     return { token: res.json("accessToken") };
 }
