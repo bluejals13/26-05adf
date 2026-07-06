44393ffaf3d4f2a48fd70816703a73acadb0a217
bluejals13 <yoriya10113@gmail.com>
2026-07-06 22:20:03 +0900

    Update user.flow.js

--- APMS.SR/k6/scenarios/user.flow.js
+++ APMS.SR/k6/scenarios/user.flow.js
@@ -6,5 +6,12 @@ export default function (data) {
     const token = data.token;
     
     const res = UserAPI.getUsers(token);
-    if (!res) console.log(res.status); return;
+    if (!res) console.log({
+      method,
+      path,
+      tokenType: typeof token,
+      bodyType: typeof body
+    });
+    
+    return;
 }
