e300ad35af02f3c89558ad96c3aaf5954b4e1bde
bluejals13 <yoriya10113@gmail.com>
2026-07-06 20:00:54 +0900

    Update run.js

--- APMS.SR/k6/run.js
+++ APMS.SR/k6/run.js
@@ -3,10 +3,20 @@
 import flow from "./core/active.js";
 import { setup } from "./setup.js";
 
+import admin from "./scenarios/admin-flow.js";
+import user from "./scenarios/user.flow.js";
+import read from "./scenarios/read.flow.js";
+import load from "./scenarios/load.test.js";
+
 export { setup };
 
+// 환경변수로 시나리오 선택
+const scenario = __ENV.SCENARIO || "load";
+
+const flows = { admin, user, read, load };
+
 export default function (data) {
-    flow(data);
+    flows[scenario](data);
 }
 
 export const options = {
