b6e9aef93db8e7d8791e2e3c37453a3a3fdfd77d
bluejals13 <yoriya10113@gmail.com>
2026-07-06 21:30:29 +0900

    Update request.js

--- APMS.SR/k6/core/request.js
+++ APMS.SR/k6/core/request.js
@@ -4,14 +4,23 @@ import http from "k6/http";
 import { config } from "../config/env.js";
 
 export default function request(method, path, token, body = null, options = {}) {
-    const payload = body ? JSON.stringify(body) : null;
+    
+    if (!config.baseUrl) { throw new Error("BASE_URL is not defined"); }
+    
+    const payload = body && typeof body === "object" ? JSON.stringify(body) : body;
     
     const headers = {
         "Content-Type": "application/json",
         ...(token && { Authorization: `Bearer ${token}`, }),
         ...(options.headers ?? {}),
     };
-
-    return http.request(method, `${config.baseUrl}${path}`, payload, {
-        ...options, headers, });
+    
+    const mergedOptions = {
+        tags: options.tags ?? {},
+        timeout: options.timeout,
+        redirects: options.redirects,
+        headers,
+    };
+    
+    return http.request(method, `${config.baseUrl}${path}`, payload, mergedOptions );
 }
