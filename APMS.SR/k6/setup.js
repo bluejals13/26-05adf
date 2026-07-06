5cc1ed7ceafecb3b61b4d8e9f7cf3a48eda13f11
bluejals13 <yoriya10113@gmail.com>
2026-07-06 21:48:35 +0900

    Update setup.js

--- APMS.SR/k6/setup.js
+++ APMS.SR/k6/setup.js
@@ -5,7 +5,9 @@ import { login } from "./api/auth.api.js";
 export function setup() {
     const res = login("test", "1378");
     
-    const token = res.json("token"); // ⭐ 핵심
+    const body = res.json();   // ✅ 여기서 파싱
+    
+    const token = body.json("token"); // ⭐ 핵심
     
     return { token };
 }
