d4a818a57a862260178965b44a413b32781ed397
bluejals13 <yoriya10113@gmail.com>
2026-07-09 23:45:09 +0900

    Update and rename EmptyLayout.tsx to AuthLayout.tsx

---
+++ APMS.SR/dev/frontend/src/layout/AuthLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet, Link } from 'react-router-dom'
+
+export default function AuthLayout() {
+  return (
+    <div className="auth-layout">
+      
+      <main className="auth-content">
+        <Outlet />
+      </main>
+
+    </div>
+  );
+}
