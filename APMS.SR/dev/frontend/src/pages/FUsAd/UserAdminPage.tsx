62221a8a45e7f4a4dd9dc4aea173677ef32bb3e4
bluejals13 <yoriya10113@gmail.com>
2026-07-10 00:45:17 +0900

    Update UserAdminPage.tsx

--- APMS.SR/dev/frontend/src/pages/FUsAd/UserAdminPage.tsx
+++ APMS.SR/dev/frontend/src/pages/FUsAd/UserAdminPage.tsx
@@ -129,7 +129,7 @@ export default function UserAdminPage() {
       {/* DELETE PENDING */}
       {pendingUsers.length > 0 && (
         <>
-          <h2 className={styles.sectionTitle}>삭제 대기 장소</h2>
+          <h1 className={styles.sectionTitle}>삭제 대기 장소</h1>
 
           {pendingUsers.map((u) => (
             <div key={u.id} className={styles.pendingRow}>
