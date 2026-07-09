5fbf02c6d5b4277d3776ab8e96c1eacb98711fe0
bluejals13 <yoriya10113@gmail.com>
2026-07-09 21:47:49 +0900

    Update Header.tsx

--- APMS.SR/dev/frontend/src/components/Header.tsx
+++ APMS.SR/dev/frontend/src/components/Header.tsx
@@ -34,7 +34,7 @@ export default function Header() {
       
       <div className="headerInner">
     
-        <div className="auth">
+        <div className="auth">  {/* 로그인 정보 */}
     
           {isLoggedIn ? (
             <>
