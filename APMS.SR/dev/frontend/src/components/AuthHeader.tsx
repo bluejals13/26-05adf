a6419faf1550a49b0927582279fd28a2cd2f2d78
bluejals13 <yoriya10113@gmail.com>
2026-07-12 22:53:11 +0900

    Update AuthHeader.tsx

--- APMS.SR/dev/frontend/src/components/AuthHeader.tsx
+++ APMS.SR/dev/frontend/src/components/AuthHeader.tsx
@@ -8,10 +8,11 @@ export default function AuthHeader(){
  return (
    <header className="header">
      <div className="headerInner">
-       <div className="auth">
-        <Link className="underline" to="/"> Main </Link>
+       <div className="auth flex items-center gap-4">
         <Link className="underline" to="/login"> Login </Link>
         <Link className="underline" to="/signup"> Sign up </Link>
+
+        <Link className="underline ml-auto" to="/"> Main </Link>
        </div>
      </div>
    </header>
