5c1d96dcc66e4e6f645cf620ec395ad1e578cd86
bluejals13 <yoriya10113@gmail.com>
2026-07-20 01:21:17 +0900

    Update AdminRoute.tsx

--- APMS.SR/dev/frontend/src/components/AdminRoute.tsx
+++ APMS.SR/dev/frontend/src/components/AdminRoute.tsx
@@ -5,14 +5,27 @@ import FullPageSpinner from "./loading/FullPageSpinner";
 import { useMe } from "../queries/useMe";
 import { usePermissions } from "../auth/hooks/usePermissions";
 
-export default function AdminRoute({ permission }: { permission?: string }) {
+interface ProtectedRouteProps {
+  role?: string;
+  permission?: string;
+}
+
+export default function ProtectedRoute({
+  role,
+  permission,
+}: ProtectedRouteProps) {
   const { data: me, isLoading } = useMe();
-  const { hasPermission, isAdmin } = usePermissions(me);
+  const { hasRole, hasPermission } = usePermissions(me);
 
   if (isLoading) return <FullPageSpinner />;
-  if (!me) return <Navigate to="/login" replace />;
 
-  if (!isAdmin()) return <Navigate to="/403" replace />;
+  if (!me) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (role && !hasRole(role)) {
+    return <Navigate to="/403" replace />;
+  }
 
   if (permission && !hasPermission(permission)) {
     return <Navigate to="/403" replace />;
