a2f11b583e3b5c0a9dd5293116e140e42d18f2b8
bluejals13 <yoriya10113@gmail.com>
2026-08-04 22:57:56 +0900

    Update AuthController.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/AuthController.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/AuthController.java
@@ -11,9 +11,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.authentication.BadCredentialsException;
 
 @RestController
 @RequestMapping("/api/auth")
