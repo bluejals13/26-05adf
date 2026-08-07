ccbe96f575ee6c9048649a910018b1a7d866bcdc
bluejals13 <yoriya10113@gmail.com>
2026-08-07 22:52:08 +0900

    Refactor SecurityIntegrationTest for clarity

--- APMS.SR/dev/backend/src/test/java/com/example/demo/auth/security/SecurityIntegrationTest.java
+++ APMS.SR/dev/backend/src/test/java/com/example/demo/auth/security/SecurityIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.security.test.context.support.WithMockUser;
 
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
