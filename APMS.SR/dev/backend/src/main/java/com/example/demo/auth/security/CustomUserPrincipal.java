9100feaa0406691d6138b2583e5d6fd668639a69
bluejals13 <yoriya10113@gmail.com>
2026-08-08 00:19:32 +0900

    Update CustomUserPrincipal.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/CustomUserPrincipal.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/CustomUserPrincipal.java
@@ -6,5 +6,19 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class CustomUserPrincipal {
-    implements UserDetails;
+    implements UserDetails{
+
+        private final Long userId;
+
+        @Override
+        public Collection<? extends GranteAuthority> getAuthorities(){
+            return Authorities; }
+
+        @Override
+        public String getUsername(){
+            return userId.toString(); }
+
+        @Override
+        public boolean isEnabled(){
+            return true; }
 }
