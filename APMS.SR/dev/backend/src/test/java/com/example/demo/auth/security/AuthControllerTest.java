6c8e3b24ea26b14e4b71ca3ba9df898664273aa3
bluejals13 <yoriya10113@gmail.com>
2026-08-07 23:56:22 +0900

    Update AuthControllerTest.java

--- APMS.SR/dev/backend/src/test/java/com/example/demo/auth/security/AuthControllerTest.java
+++ APMS.SR/dev/backend/src/test/java/com/example/demo/auth/security/AuthControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcTest;
 
 import org.springframework.data.redis.core.RedisTemplate;
 
