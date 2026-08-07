8fbb8b9d9d0985f74040f9aadb412c622200845a
bluejals13 <yoriya10113@gmail.com>
2026-08-07 23:17:11 +0900

    Update AuthControllerTest.java

--- APMS.SR/dev/backend/src/test/java/com/example/demo/auth/security/AuthControllerTest.java
+++ APMS.SR/dev/backend/src/test/java/com/example/demo/auth/security/AuthControllerTest.java
@@ -13,7 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+    
 import org.springframework.http.MediaType;
 
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AuthControllerTest {
 
 
