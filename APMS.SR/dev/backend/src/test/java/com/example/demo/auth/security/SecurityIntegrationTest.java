e5f41aef07e184117348f1ada89b971eb72b2343
bluejals13 <yoriya10113@gmail.com>
2026-08-08 19:15:33 +0900

    Update SecurityIntegrationTest.java

--- APMS.SR/dev/backend/src/test/java/com/example/demo/auth/security/SecurityIntegrationTest.java
+++ APMS.SR/dev/backend/src/test/java/com/example/demo/auth/security/SecurityIntegrationTest.java
@@ -24,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@Sql("/sql/security-test-data.sql")
 class SecurityIntegrationTest {
 
     @Autowired
