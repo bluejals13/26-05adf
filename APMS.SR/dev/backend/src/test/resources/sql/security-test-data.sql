af2817dfb1c72c63ad16b757a9679384338d58fb
bluejals13 <yoriya10113@gmail.com>
2026-08-08 19:21:04 +0900

    Update security-test-data.sql

--- APMS.SR/dev/backend/src/test/resources/sql/security-test-data.sql
+++ APMS.SR/dev/backend/src/test/resources/sql/security-test-data.sql
@@ -1,29 +1,16 @@
-INSERT INTO roles (id, name, description, level, is_system)
-VALUES
-(1, 'ADMIN', 'Administrator', 10, false),
-(2, 'USER', 'Normal User', 10, false);
-
-INSERT INTO permissions (id, name, description)
-VALUES
-(1, 'USER_READ', NULL),
-(2, 'USER_CREATE', NULL),
-(3, 'USER_UPDATE', NULL),
-(4, 'USER_STATUS_UPDATE', NULL),
-(5, 'USER_DELETE', NULL),
-(6, 'ROLE_READ', NULL),
-(7, 'ROLE_CREATE', NULL),
-(8, 'ROLE_UPDATE', NULL),
-(9, 'ROLE_DELETE', NULL),
-(10, 'ROLE_ASSIGN', NULL),
-(11, 'ROLE_REMOVE', NULL),
-(12, 'MENU_READ', NULL),
-(13, 'MENU_CREATE', NULL),
-(14, 'MENU_UPDATE', NULL),
-(15, 'MENU_DELETE', NULL),
-(16, 'PERMISSION_READ', NULL),
-(17, 'PERMISSION_CREATE', NULL),
-(18, 'PERMISSION_UPDATE', NULL),
-(19, 'PERMISSION_DELETE', NULL);
+INSERT INTO users (
+    id,
+    username,
+    password,
+    status,
+    email
+) VALUES (
+    1,
+    'testuser',
+    '$2a$10$eFZu4rKvdovHaBfx0w91Pe8bxWl.GNOQA5i2NP2rgHq1.ydzGY2je',
+    'ACTIVE',
+    'testuser@test.com'
+);
 
 INSERT INTO users (
     id,
@@ -31,16 +18,7 @@ INSERT INTO users (
     password,
     status,
     email
-)
-VALUES
-(
-    1,
-    'testuser',
-    '$2a$10$eFZu4rKvdovHaBfx0w91Pe8bxWl.GNOQA5i2NP2rgHq1.ydzGY2je',
-    'ACTIVE',
-    'testuser@test.com'
-),
-(
+) VALUES (
     2,
     'admin',
     '$2a$10$mLxWbOKAz6/EAO14u.SQB.vtZTwQBt3kQb0qeGMdwm2nnvoX8Pwji',
@@ -49,6 +27,11 @@ VALUES
 );
 
 INSERT INTO user_roles (user_id, role_id)
-VALUES
-(1, 2),
-(2, 1);
+SELECT 1, id
+FROM roles
+WHERE name = 'USER';
+
+INSERT INTO user_roles (user_id, role_id)
+SELECT 2, id
+FROM roles
+WHERE name = 'ADMIN';
