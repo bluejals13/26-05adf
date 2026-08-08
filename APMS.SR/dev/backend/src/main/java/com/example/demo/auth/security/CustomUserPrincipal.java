446e8284ed0e09b04a7b5da3aba885ca2a1cf593
bluejals13 <yoriya10113@gmail.com>
2026-08-08 14:32:40 +0900

    Update CustomUserPrincipal.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/CustomUserPrincipal.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/CustomUserPrincipal.java
@@ -7,17 +7,17 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class CustomUserPrincipal implements UserDetails {
 
     private final Long userId;
-    private final Collection<? extends GrantedAuthority> authorities;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
+        return List.of();
     }
 
     @Override
