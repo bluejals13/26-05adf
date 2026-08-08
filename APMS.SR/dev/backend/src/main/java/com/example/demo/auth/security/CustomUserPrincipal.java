699cd33c5d8ed0b1bd01c3130a6301814fc09c94
bluejals13 <yoriya10113@gmail.com>
2026-08-08 14:32:06 +0900

    Update CustomUserPrincipal.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/CustomUserPrincipal.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/auth/security/CustomUserPrincipal.java
@@ -3,22 +3,50 @@ package com.example.demo.auth.security;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
 @Getter
 @AllArgsConstructor
-public class CustomUserPrincipal {
-    implements UserDetails{
+public class CustomUserPrincipal implements UserDetails {
 
-        private final Long userId;
+    private final Long userId;
+    private final Collection<? extends GrantedAuthority> authorities;
 
-        @Override
-        public Collection<? extends GranteAuthority> getAuthorities(){
-            return Authorities; }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
 
-        @Override
-        public String getUsername(){
-            return userId.toString(); }
+    @Override
+    public String getUsername() {
+        return userId.toString();
+    }
 
-        @Override
-        public boolean isEnabled(){
-            return true; }
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
