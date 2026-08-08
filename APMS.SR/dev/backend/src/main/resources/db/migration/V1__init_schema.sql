d9a6b8ed374b273d81ebaf73ca79c727912644a1
bluejals13 <yoriya10113@gmail.com>
2026-08-08 15:30:47 +0900

    Update V1__init_schema.sql

--- APMS.SR/dev/backend/src/main/resources/db/migration/V1__init_schema.sql
+++ APMS.SR/dev/backend/src/main/resources/db/migration/V1__init_schema.sql
@@ -12,7 +12,6 @@ CREATE TABLE users (
     email VARCHAR(255)
 );
 
-
 CREATE TABLE roles (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
@@ -21,61 +20,8 @@ CREATE TABLE roles (
     is_system BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-
 CREATE TABLE permissions (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(255),
     name VARCHAR(100) NOT NULL UNIQUE
-);
-
-
-CREATE TABLE user_roles (
-    user_id BIGINT NOT NULL,
-    role_id BIGINT NOT NULL,
-
-    PRIMARY KEY(user_id, role_id),
-
-    CONSTRAINT fk_user_roles_user
-        FOREIGN KEY(user_id)
-        REFERENCES users(id),
-
-    CONSTRAINT fk_user_roles_role
-        FOREIGN KEY(role_id)
-        REFERENCES roles(id)
-);
-
-
-CREATE TABLE role_permissions (
-    role_id BIGINT NOT NULL,
-    permission_id BIGINT NOT NULL,
-
-    PRIMARY KEY(role_id, permission_id),
-
-    CONSTRAINT fk_role_permissions_role
-        FOREIGN KEY(role_id)
-        REFERENCES roles(id),
-
-    CONSTRAINT fk_role_permissions_permission
-        FOREIGN KEY(permission_id)
-        REFERENCES permissions(id)
-);
-
-
-CREATE TABLE menu (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    price INT NOT NULL
-);
-
-
-CREATE TABLE audit_logs (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    action VARCHAR(100),
-    created_at DATETIME(6),
-    result VARCHAR(255),
-    target_id BIGINT,
-    target_type VARCHAR(255),
-    user_id BIGINT,
-    after_value VARCHAR(255),
-    before_value VARCHAR(255)
 );
