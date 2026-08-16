c83daff7e970e7967270d353a3943bcca101033d
bluejals13 <yoriya10113@gmail.com>
2026-08-16 11:02:53 +0900

    Update PermissionDetailResponse.java

--- APMS.SR/dev/backend/src/main/java/com/example/demo/iam/permission/dto/PermissionDetailResponse.java
+++ APMS.SR/dev/backend/src/main/java/com/example/demo/iam/permission/dto/PermissionDetailResponse.java
@@ -5,21 +5,17 @@ import java.util.List;
 import com.example.demo.iam.permission.domain.Permission;
 import com.example.demo.iam.role.domain.Role;
 import com.example.demo.iam.role.dto.RoleDto;
-import com.example.demo.iam.user.domain.User;
-import com.example.demo.iam.user.dto.UserDto;
 
 public record PermissionDetailResponse(
-    Long id,
-    String name,
-    String description,
-    List<RoleDto> roles,
-    List<UserDto> users
+        Long id,
+        String name,
+        String description,
+        List<RoleDto> roles
 ) {
 
     public static PermissionDetailResponse from(
             Permission permission,
-            List<Role> roles,
-            List<User> users
+            List<Role> roles
     ) {
 
         return new PermissionDetailResponse(
@@ -28,9 +24,6 @@ public record PermissionDetailResponse(
                 permission.getDescription(),
                 roles.stream()
                         .map(RoleDto::from)
-                        .toList(),
-                users.stream()
-                        .map(UserDto::from)
                         .toList()
         );
     }
