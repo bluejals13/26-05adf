44da1939555540ad23a1fca6cae8f0b8bd9ecfa6
bluejals13 <yoriya10113@gmail.com>
2026-08-17 09:58:51 +0900

    Update V4__insert_permissions.sql

--- APMS.SR/dev/backend/src/main/resources/db/migration/V4__insert_permissions.sql
+++ APMS.SR/dev/backend/src/main/resources/db/migration/V4__insert_permissions.sql
@@ -1,22 +1,21 @@
-INSERT INTO permissions(name)
+INSERT INTO permissions(name, description)
 VALUES
-('USER_READ'),
-('USER_STATUS_UPDATE'),
-('USER_DELETE'),
+('USER_READ', '사용자 조회'),
+('USER_STATUS_UPDATE', '사용자 상태 변경'),
+('USER_DELETE', '사용자 삭제'),
 
-('ROLE_READ'),
-('ROLE_CREATE'),
-('ROLE_UPDATE'),
-('ROLE_DELETE'),
+('ROLE_READ', 'Role 조회'),
+('ROLE_CREATE', 'Role 생성'),
+('ROLE_UPDATE', 'Role 수정'),
+('ROLE_DELETE', 'Role 삭제'),
+('ROLE_ASSIGN', 'Role에 Permission 할당'),
 
-('ROLE_ASSIGN'),
+('MENU_READ', '메뉴 조회'),
+('MENU_CREATE', '메뉴 생성'),
+('MENU_UPDATE', '메뉴 수정'),
+('MENU_DELETE', '메뉴 삭제'),
 
-('MENU_READ'),
-('MENU_CREATE'),
-('MENU_UPDATE'),
-('MENU_DELETE'),
-
-('PERMISSION_READ');
+('PERMISSION_READ', 'Permission 조회');
 
 
 INSERT INTO role_permissions(role_id, permission_id)
