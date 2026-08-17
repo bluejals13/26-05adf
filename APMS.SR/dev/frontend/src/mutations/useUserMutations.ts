b80e47ba819a65edf9c4db6991639b790cff7052
bluejals13 <yoriya10113@gmail.com>
2026-08-17 18:17:21 +0900

    Update useUserMutations.ts

--- APMS.SR/dev/frontend/src/mutations/useUserMutations.ts
+++ APMS.SR/dev/frontend/src/mutations/useUserMutations.ts
@@ -3,21 +3,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { userApi } from "../api/user.api";
-
 import { userKeys } from "../queries/useUsers";
-import { authKeys } from "../auth/auth.keys";
-
 import type { UserStatus } from "../auth/auth.types";
+import { authKeys } from "../auth/auth.keys";
 
 export function useUserMutations() {
   const qc = useQueryClient();
 
-  const refreshUserData = async () => {
+  const invalidateUserQueries = async () => {
     await Promise.all([
       qc.invalidateQueries({
         queryKey: userKeys.all,
       }),
-
       qc.invalidateQueries({
         queryKey: authKeys.me(),
       }),
@@ -33,13 +30,14 @@ export function useUserMutations() {
       status: UserStatus;
     }) => userApi.changeStatus(id, status),
 
-    onSuccess: refreshUserData,
+    onSuccess: invalidateUserQueries,
   });
 
   const deleteUser = useMutation({
-    mutationFn: (id: number) => userApi.deleteUser(id),
+    mutationFn: (id: number) =>
+      userApi.deleteUser(id),
 
-    onSuccess: refreshUserData,
+    onSuccess: invalidateUserQueries,
   });
 
   return {
@@ -47,4 +45,3 @@ export function useUserMutations() {
     deleteUser,
   };
 }
-
