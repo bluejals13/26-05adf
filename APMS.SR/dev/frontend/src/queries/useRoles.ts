f2f5bb38d48bbedf04a3517e5c4e5f8255804a9b
bluejals13 <yoriya10113@gmail.com>
2026-08-17 18:17:41 +0900

    Update useRoles.ts

--- APMS.SR/dev/frontend/src/queries/useRoles.ts
+++ APMS.SR/dev/frontend/src/queries/useRoles.ts
@@ -1,6 +1,7 @@
 // queries/useRoles.ts
 
 import { useQuery } from "@tanstack/react-query";
+
 import { fetchRoles } from "../api/role.api";
 import type { Role } from "../queries/role";
 
@@ -8,7 +9,10 @@ export const useRoles = () =>
   useQuery<Role[]>({
     queryKey: ["roles"],
     queryFn: fetchRoles,
+
     initialData: [],
+
+    // 관리자 페이지에서는 최신 RBAC 상태 우선
     staleTime: 0,
     refetchOnMount: "always",
   });
