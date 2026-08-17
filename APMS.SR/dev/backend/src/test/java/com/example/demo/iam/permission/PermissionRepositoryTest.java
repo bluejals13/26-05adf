167ce483d67707ed234c32355bbfc34492359481
bluejals13 <yoriya10113@gmail.com>
2026-08-17 11:16:46 +0900

    Update PermissionRepositoryTest.java

--- APMS.SR/dev/backend/src/test/java/com/example/demo/iam/permission/PermissionRepositoryTest.java
+++ APMS.SR/dev/backend/src/test/java/com/example/demo/iam/permission/PermissionRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.iam.permission.repository;
+package com.example.demo.iam.permission;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
